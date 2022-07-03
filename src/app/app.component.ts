d731476ecc377ca3a2d43d3d26c34ac48cb377a8
Giame Fajardo <giame_fajardo@cecomsa.com>
2022-07-02 23:38:34 -0400

    [IMP] my-store: Showing products

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-store';
+
+  products: Product[] = [
+    {
+      name: 'EL mejor juguete',
+      price: 565,
+      image: 'https://picsum.photos/200/200',
+      category: 'all',
+    },
+    {
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: 'https://picsum.photos/200/200'
+    },
+    {
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: 'https://picsum.photos/200/200'
+    },
+    {
+      name: 'Mis libros',
+      price: 23,
+      image: 'https://picsum.photos/200/200'
+    },
+    {
+      name: 'Casa para perro',
+      price: 34,
+      image: 'https://picsum.photos/200/200'
+    },
+    {
+      name: 'Gafas',
+      price: 3434,
+      image: 'https://picsum.photos/200/200'
+    }
+  ]
 }
